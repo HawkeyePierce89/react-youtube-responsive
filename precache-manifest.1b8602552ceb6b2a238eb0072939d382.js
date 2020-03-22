999e0bcb9a0b1d077222fa3e0e993feabb933327
Anton Karmanov <antonkarmanov@MacBook-Pro-Anton.local>
2020-03-22 13:48:32 +0300

    Updates

---
+++ precache-manifest.1b8602552ceb6b2a238eb0072939d382.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb47c0d0ef3753e20de5874559a7d725",
+    "url": "/react-youtube-responsive/index.html"
+  },
+  {
+    "revision": "a0219427831cb9977ee4",
+    "url": "/react-youtube-responsive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "55de9bd6ce0d58df0ede",
+    "url": "/react-youtube-responsive/static/js/2.74ba91c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube-responsive/static/js/2.74ba91c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0219427831cb9977ee4",
+    "url": "/react-youtube-responsive/static/js/main.ce7f2212.chunk.js"
+  },
+  {
+    "revision": "1f5072b7109ab991ef0e",
+    "url": "/react-youtube-responsive/static/js/runtime-main.60a47245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-youtube-responsive/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-youtube-responsive/precache-manifest.1b8602552ceb6b2a238eb0072939d382.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-youtube-responsive/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
